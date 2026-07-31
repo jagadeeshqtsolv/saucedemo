a9f51c139233e56d30e8e1a8f2fdc29d4a55fce7
jagadeeshqtsolv <105221373+jagadeeshqtsolv@users.noreply.github.com>
2026-07-31 09:50:27 +0530

    Update api-testing.spec.ts

--- tests/api/api-testing.spec.ts
+++ tests/api/api-testing.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@support/fixtures';
 import env from '@support/env';
-import { Login } from '../../api/Login';
-import { UserDetails } from '../../api/UserDetails';
 
 test('API Testing', { tag: ["@api","@P1","@case-386395d6-b446-4e19-a9c8-b9141e1f3994","@req-0dd15af8-b021-455e-b3e8-e6526ef4326b"] }, async ({ page, apiFlowsPage, request }) => {
   const vars: Record<string, string> = {};
